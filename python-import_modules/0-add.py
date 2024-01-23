17d427be0c5f742cd450338231af610939253837
Spacefox95 <nathan.raynal@gmail.com>
2024-01-23 14:48:30 +0100

    ok

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-c = add(a, b)
-print("{} + {} = {}".format(a, b, c))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    c = add(a, b)
+    print("{} + {} = {}".format(a, b, c))
