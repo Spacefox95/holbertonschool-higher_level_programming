04776e4d48973b35b3a51620f817792e1e134730
Spacefox95 <nathan.raynal@gmail.com>
2024-01-23 10:15:03 +0100

    testé

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(00, 99):
-	print("{:02d}".format(i), end=", ")
-print("99")
+for i in range(0, 8):
+    for b in range(i + 1, 10):
+        print("{}".format(i), end="")
+        print("{}".format(b), end=", ")
+print("89")
