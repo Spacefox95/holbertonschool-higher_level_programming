c617903330c7d5e192efdb0a747eeca6f89d4191
Spacefox95 <nathan.raynal@gmail.com>
2024-01-29 09:07:10 +0100

    k

--- python-data_structures/4-main.py
+++ python-data_structures/4-main.py
@@ -2,7 +2,7 @@
 new_in_list = __import__('4-new_in_list').new_in_list
 
 my_list = [1, 2, 3, 4, 5]
-idx = 3
+idx = 10
 new_element = 9
 new_list = new_in_list(my_list, idx, new_element)
 
--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     nlist = my_list.copy()
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(nlist):
         return nlist
     else:
         nlist[idx] = element
