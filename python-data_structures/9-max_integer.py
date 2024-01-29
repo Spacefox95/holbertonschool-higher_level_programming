8ab86f1f412fdb4d1cb860b75d8f3ffdae0bdbae
Spacefox95 <nathan.raynal@gmail.com>
2024-01-29 14:09:32 +0100

    ok

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = my_list[0]
     if my_list:
+        max = my_list[0]
         for i in my_list:
             if i > max:
                 max = i
