ff351ca2883f9fe3ca135523eddc1252c1877e82
Spacefox95 <nathan.raynal@gmail.com>
2024-01-29 15:20:38 +0100

    ok

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     boolean = []
-    for i in my_list:
+    for i in range(len(my_list)):
         boolean.append(i % 2 == 0)
     return boolean
