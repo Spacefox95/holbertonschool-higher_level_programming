2adaacdbeb894d471fafad2bef7de19d260d5bce
Spacefox95 <nathan.raynal@gmail.com>
2024-01-29 10:43:41 +0100

    ok

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        print(' '.join(map(str, row)).format(row))
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print(matrix[i][j], end=' ')
+        print()
