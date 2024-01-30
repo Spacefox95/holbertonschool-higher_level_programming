3ac781a2f74506bc3b8ac649fd83a5bb429b1a67
Spacefox95 <nathan.raynal@gmail.com>
2024-01-30 14:46:41 +0100

    ok

--- python-more_data_structures/6-main.py
+++ python-more_data_structures/6-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-a_dictionary = { 'language': "C", 'Number': 89, 'Number': 18, 'track': 89, 'ids': [1, 2, 3] }
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
 print_sorted_dictionary(a_dictionary)
--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
+
 def print_sorted_dictionary(a_dictionary):
     sort_dict = dict(sorted(a_dictionary.items()))
+
     for key, value in sort_dict.items():
         print("{} : {}".format(key, value))
