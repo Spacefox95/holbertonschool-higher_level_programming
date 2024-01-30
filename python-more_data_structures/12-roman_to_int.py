de0bf3060314fc2a8a5b4423801738cda2bd7e1c
Spacefox95 <nathan.raynal@gmail.com>
2024-01-30 16:16:14 +0100

    ok

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     a = 0
-    if roman_string:
+    if isinstance(roman_string, str) is True:
         num = {'I': 1, 'V': 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         for i in roman_string:
             for key in num:
