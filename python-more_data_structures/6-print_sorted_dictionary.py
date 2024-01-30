9961664f8cae506b7b04ca022b72c153dc6ff040
Spacefox95 <nathan.raynal@gmail.com>
2024-01-30 13:46:21 +0100

    ok

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary is not None:
-        for key, value in sorted(a_dictionary.items(), key=lambda x : x[0]):
+        for key, value in sorted(a_dictionary.items(), key=lambda x: x[0]):
             print("{} : {}".format(key, value))
