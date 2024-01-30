e365b27590a78e91897c62ca5968ed351e1af962
Spacefox95 <nathan.raynal@gmail.com>
2024-01-30 10:15:47 +0100

    ok

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        uniq = set(my_list)
-        result = sum(uniq)
-        return result
+    uniq = set(my_list)
+    result = sum(uniq)
+    return result
