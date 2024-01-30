e1df2d6bf5476c2b14e5c394ef370a6b715f467c
Spacefox95 <nathan.raynal@gmail.com>
2024-01-30 15:21:24 +0100

    ok

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,4 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = dict(map(lambda x: (x[0], x[1] * 2),
-                        a_dictionary.items()))
+    new_dict = {key: value * 2 for key, value in a_dictionary.items()}
     return new_dict
