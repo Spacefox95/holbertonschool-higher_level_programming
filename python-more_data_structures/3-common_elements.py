f340f60ec3207708344f3b9e8a61e44a84430187
Spacefox95 <nathan.raynal@gmail.com>
2024-01-30 10:29:12 +0100

    ok

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                return i
-            else:
-                return ""
+    return set_1 & set_2
