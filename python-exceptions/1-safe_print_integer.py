8bb30ff07a3c7515055899037119ea7af37430ce
Spacefox95 <nathan.raynal@gmail.com>
2024-01-31 12:57:01 +0100

    ok

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except ValueError:
+    if value:
+        try:
+            print("{:d}".format(value))
+            return True
+        except ValueError:
+            return False
+    else:
         return False
