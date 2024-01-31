fdf67e76241fec41f62babf9ed839334e3045d0c
Spacefox95 <nathan.raynal@gmail.com>
2024-01-31 13:02:42 +0100

    ok

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,7 @@ def safe_print_integer(value):
         try:
             print("{:d}".format(value))
             return True
-        except ValueError:
+        except (ValueError, TypeError):
             return False
     else:
         return False
