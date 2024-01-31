8282c436c1830ffb13559a14ce3fff4e62464a09
Spacefox95 <nathan.raynal@gmail.com>
2024-01-31 13:19:55 +0100

    ok

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if value:
-        try:
-            print("{:d}".format(value))
-            return True
-        except (ValueError, TypeError, SyntaxError):
-            return False
-    else:
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
         return False
