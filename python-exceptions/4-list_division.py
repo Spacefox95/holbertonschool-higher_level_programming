7fdc3ebc4c7abc57052cde52df3123bf403a81c0
Spacefox95 <nathan.raynal@gmail.com>
2024-02-02 12:05:15 +0100

    ok

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,8 +5,14 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         sum = 0
         try:
-            num1 = my_list_1[i]
-            num2 = my_list_2[i]
+            if num1 < len(my_list_1):
+                num1 = my_list_1[i]
+            else:
+                num1 = 0
+            if num2 < len(my_list_2):
+                num2 = my_list_2[i]
+            else:
+                num2 = 0
             sum = num1 / num2
         except ZeroDivisionError:
             print("division by 0")
