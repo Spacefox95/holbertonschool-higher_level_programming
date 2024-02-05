eb89ffe00d6a4bf88da74b81116f500b0bdd1676
Spacefox95 <nathan.raynal@gmail.com>
2024-02-05 10:45:59 +0100

    ok

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!/bin/usr/python
-'''
+"""
 This module defines the square class
-'''
+"""
 
 
 class Square:
-    ''' For now, an empty class with no attributes or methods '''
+    """ For now, an empty class with no attributes or methods """
     pass
