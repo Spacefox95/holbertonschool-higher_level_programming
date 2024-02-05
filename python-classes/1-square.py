1006cf3f572f2b128f5c62d0142995e6fe1cada2
Spacefox95 <nathan.raynal@gmail.com>
2024-02-05 15:37:58 +0100

    ok

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Square : This module defines the Square class
+It includes a size attribute
+"""
+
+
+class Square:
+    """
+    Square : For now, an empty class with no attributes or methods.
+    Attributes: Size is the size of the square.
+
+    """
+    def __init__(self, size):
+        self.__size = size
