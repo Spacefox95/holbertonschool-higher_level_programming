8c9e613fbd899fc62d3c95546ea783c9411d3896
Spacefox95 <nathan.raynal@gmail.com>
2024-02-05 16:08:11 +0100

    ok

---
+++ python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Square : This module defines the Square class
+It includes a size attribute
+"""
+
+
+class Square:
+    """
+    Square : Class that represents a square
+    Attributes: 
+    - size (int): the size of the square.
+
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a square instance.
+        Parameterd:
+        - size (int) : the size of the square. Default is 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
