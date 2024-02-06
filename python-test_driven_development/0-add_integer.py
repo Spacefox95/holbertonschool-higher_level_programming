3aca318d85843eba585eb303b4ac46b2a7257098
Spacefox95 <nathan.raynal@gmail.com>
2024-02-06 15:20:54 +0100

    ok

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+add_integer = __import__('0-add_integer').add_integer
+create a add_integer variable
+import add_integer function
+"""
+
+
+def add_integer(a, b=98):
+    """
+    add_integer: add two integer of floats; if floats, casted in int
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
