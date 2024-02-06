12f3f694955436b5392318ccd8cf941755d7843f
Spacefox95 <nathan.raynal@gmail.com>
2024-02-06 11:58:37 +0100

    ok

--- python-classes/6-main.py
+++ python-classes/6-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 Square = __import__('6-square').Square
 
-my_square_1 = Square(3)
+my_square_1 = Square("b")
 my_square_1.my_print()
 
 print("--")
--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 """
 Square : This module defines the Square class
 It includes a size attribute
