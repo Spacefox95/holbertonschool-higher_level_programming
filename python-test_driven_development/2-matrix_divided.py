cce4ef7e418bc766a7b68043ecabf28276f6f162
Spacefox95 <nathan.raynal@gmail.com>
2024-02-08 09:37:19 +0100

    ok

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -15,8 +15,8 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if not all(isinstance(i, (int, float)) for row in matrix for i in row):
-        raise TypeError("matrix must\
-                         be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must \
+be a matrix (list of lists) of integers/floats")
     if len(set(map(len, matrix))) != 1:
         raise TypeError("Each row of the matrix must have the same size")
 
