ce35e40025a75a3c5bdff9aff88e8f32bf71905d
Spacefox95 <nathan.raynal@gmail.com>
2024-02-09 10:50:13 +0100

    ok

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -123,9 +123,9 @@ class Rectangle:
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
+        elif not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
+        elif rect_1.area() >= rect_2.area():
             return rect_1
         else:
             return rect_2
