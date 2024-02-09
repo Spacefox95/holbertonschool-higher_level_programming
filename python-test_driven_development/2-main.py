c9187ad0de6f857e677ca7e44b242df990ce8a9b
Spacefox95 <nathan.raynal@gmail.com>
2024-02-09 11:59:03 +0100

    ok

--- python-test_driven_development/2-main.py
+++ python-test_driven_development/2-main.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
+matrix = []
 print(matrix_divided(matrix, 3))
 print(matrix)
