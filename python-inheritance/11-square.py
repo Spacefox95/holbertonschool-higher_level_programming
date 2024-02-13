40ebf2ae1ca2927809ac8567d8a607ab0849996e
Spacefox95 <nathan.raynal@gmail.com>
2024-02-13 14:42:31 +0100

    ok

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -2,56 +2,17 @@
 
 
 """
+===========================
 Create a BaseGeometry class
+===========================
 """
-
-
-class BaseGeometry:
-    """
-    For now, an empty class
-    """
-    def area(self):
-        """
-        For now, just raise an exception
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Check is the value is correct
-        """
-        if not type(value) is int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """
-    New class that inherits from BaseGeometry
-    """
-    def __init__(self, width, height):
-        """
-        Initialize the rectangle class:
-        - width(int) is a private instance
-        - height(int) is a private instance
-        """
-        super().integer_validator("width", width)
-        self.__width = width
-        super().integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """
-        Return the area of the rectangle
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """
-        Print a rectangle description
-        """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+===========================
+Create a BaseGeometry class
+===========================
+"""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -73,3 +34,9 @@ class Square(Rectangle):
         Correct output for square
         """
         return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        """
+        Return the area of the rectangle
+        """
+        return self.__size * self.__size
