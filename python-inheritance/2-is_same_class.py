181f9810f27ce656603726f364198430e470aabd
Spacefox95 <nathan.raynal@gmail.com>
2024-02-13 10:27:20 +0100

    ok

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""
+Check that the object is an instance of a specified class
+Return True if true, else False
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Return the type of the object
+    """
+    return type(obj) is a_class
