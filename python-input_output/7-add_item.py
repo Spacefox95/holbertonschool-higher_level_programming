831d04f31ca3b70a7827d614ad2458816073cc02
Spacefox95 <nathan.raynal@gmail.com>
2024-02-15 11:20:36 +0100

    ok

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+"""
+Scripts that add all arguments to a Python list
+Then, save them to a file
+"""
+
+
 import json
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    filename = "add_item.json"
+filename = "add_item.json"
 
-    try:
-        my_obj = load_from_json_file(filename)
-    except Exception:
-        my_obj = []
+try:
+    my_obj = load_from_json_file(filename)
+except Exception:
+    my_obj = []
 
-    save_to_json_file(my_obj + sys.argv[1:], filename)
+save_to_json_file(my_obj + sys.argv[1:], filename)
