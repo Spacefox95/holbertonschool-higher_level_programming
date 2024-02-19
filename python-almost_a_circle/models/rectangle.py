129ab0a5069a55975d39237420892bf84f30dc3c
Spacefox95 <nathan.raynal@gmail.com>
2024-02-19 11:18:31 +0100

    ok

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -20,10 +20,10 @@ class Rectangle(Base):
         - y
         - id
         """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         self.id = id
         super().__init__(self.id)
 
