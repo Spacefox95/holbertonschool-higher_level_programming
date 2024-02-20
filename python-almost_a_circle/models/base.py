28cb2884d61cfe6379a3dbf657424820f581adb1
Spacefox95 <nathan.raynal@gmail.com>
2024-02-20 11:07:44 +0100

    ok

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Create a new class Base, that will be the base of our geometric forms.
 """
+import json
 
 
 class Base:
@@ -22,3 +23,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
