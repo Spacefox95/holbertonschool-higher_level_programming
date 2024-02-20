733d5d7898494693af98ee605a83b8952706b534
Spacefox95 <nathan.raynal@gmail.com>
2024-02-20 10:57:10 +0100

    ok

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -35,6 +35,17 @@ class Square(Rectangle):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
+    def to_dictionary(self):
+        """
+        Returns a dictionary representation of a rectangle
+        """
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
+
     def __str__(self):
         """
         Returns Square carateristics
