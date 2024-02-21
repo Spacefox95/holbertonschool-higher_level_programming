2f95410a36633d62875c6a2ec540a983aa772d6f
Spacefox95 <nathan.raynal@gmail.com>
2024-02-21 10:46:57 +0100

    ok

---
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Unittest for max_integer([..])
+"""
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """
+    Test the Rectangle class
+    """
+    def test_rectangle_exist_0(self):
+        """
+        Test Rectangle inheritance from Base
+        """
+        r1 = Rectangle(1, 2)
+        self.assertEqual(r1.id, 4)
+
+    def test_rectangle_exist_1(self):
+        """
+        Test Rectangle inheritance from Base
+        """
+        r1 = Rectangle(1, 2, 3)
+        self.assertEqual(r1.id, 5)
+
+    def test_rectangle_exist_2(self):
+        """
+        Test Rectangle inheritance from Base
+        """
+        r1 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(r1.id, 6)
+    
+    def test_rectangle_exist_3(self):
+        """
+        Test Rectangle inheritance from Base
+        """
+        with self.assertRaises(TypeError):
+            r1 = Rectangle("1", 2)
+    
+    def test_rectangle_exist_4(self):
+        """
+        Test Rectangle inheritance from Base
+        """
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, "2")
+
+    def test_rectangle_exist_5(self):
+        """
+        Test Rectangle inheritance from Base
+        """
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, 2, "3")
+
+    def test_rectangle_exist_6(self):
+        """
+        Test Rectangle inheritance from Base
+        """
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(1, 2, 3, "4")
