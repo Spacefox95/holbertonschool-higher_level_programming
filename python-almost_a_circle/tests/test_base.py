c30e07895321aac5e48e7aa038c859c445bdf5c5
Spacefox95 <nathan.raynal@gmail.com>
2024-02-21 09:32:24 +0100

    ko

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -11,13 +11,13 @@ class TestBase(unittest.TestCase):
         """
         Create instances of Base and assign to check id
         """
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
+        b1 = Base(12)
+        self.assertEqual(b1.id, 12)
         b2 = Base()
-        self.assertEqual(b2.id, 2)
+        self.assertEqual(b2.id, 1)
         b3 = Base()
-        self.assertEqual(b3.id, 3)
-        b4 = Base(12)
-        self.assertEqual(b4.id, 12)
+        self.assertEqual(b3.id, 2)
+        b4 = Base()
+        self.assertEqual(b4.id, 3)
         b5 = Base()
         self.assertEqual(b5.id, 4)
