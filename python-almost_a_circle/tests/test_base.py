d93473265128099980f4fef60b2d088aaf52f1cb
Spacefox95 <nathan.raynal@gmail.com>
2024-02-21 09:39:31 +0100

    ok

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -7,17 +7,29 @@ from models.base import Base
 
 
 class TestBase(unittest.TestCase):
+    """
+    Test the Base class
+    """
     def test_assign_id(self):
         """
-        Create instances of Base and assign to check id
+        Test Base instantation
         """
-        b1 = Base(12)
-        self.assertEqual(b1.id, 12)
+        b1 = Base()
+
+        self.assertEqual(b1.id, 1)
+
+    def test_incrementation(self):
+        """
+        Test Base incrementation
+        """
         b2 = Base()
-        self.assertEqual(b2.id, 1)
         b3 = Base()
-        self.assertEqual(b3.id, 2)
-        b4 = Base()
-        self.assertEqual(b4.id, 3)
-        b5 = Base()
-        self.assertEqual(b5.id, 4)
+        self.assertEqual(b3.id, b2.id + 1)
+
+    def test_assignement_89(self):
+        """
+        Test Base assignement
+        """
+        b4 = Base(89)
+        self.assertEqual(b4.id, 89)
+
