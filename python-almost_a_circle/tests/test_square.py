f5b4c120e714c80592eb8156e6931aa925d8bd7b
Spacefox95 <nathan.raynal@gmail.com>
2024-02-22 12:32:16 +0100

    ok

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -39,6 +39,65 @@ class TestSquare(unittest.TestCase):
         s1 = Square(1, 2, 3)
         self.assertIsNotNone(s1)
     
+    def test_square_exist_3(self):
+        """
+        Test Rectangle inheritance from Base
+        """
+        with self.assertRaises(TypeError):
+            s1 = Square("1")
 
+    def test_square_exist_4(self):
+        """
+        Test Rectangle inheritance from Base
+        """
+        with self.assertRaises(TypeError):
+            s1 = Square(1, "2")
 
+    def test_square_exist_5(self):
+        """
+        Test Rectangle inheritance from Base
+        """
+        with self.assertRaises(TypeError):
+            s1 = Square(1, 2, "3")
+
+    def test_square_exist_6(self):
+        """
+        Test Rectangle inheritance from Base
+        """
+        s1 = Square(1, 2, 3, 4)
+        self.assertIsNotNone(s1)
+
+    def test_square_exist_7(self):
+        """
+        Test Rectangle inheritance from Base
+        """
+        with self.assertRaises(ValueError):
+            s1 = Square(-1)
     
+    def test_square_exist_8(self):
+        """
+        Test Rectangle inheritance from Base
+        """
+        with self.assertRaises(ValueError):
+            s1 = Square(1, -2)
+    
+    def test_square_exist_7(self):
+        """
+        Test Rectangle inheritance from Base
+        """
+        with self.assertRaises(ValueError):
+            s1 = Square(1, 2, -3)
+
+    def test_square_exist_7(self):
+        """
+        Test Rectangle inheritance from Base
+        """
+        with self.assertRaises(ValueError):
+            s1 = Square(0)
+    
+    def test_str_exist(self):
+        """
+        Test the str return
+        """
+        s1 = Square(4, 6, 2, 1)
+        self.assertEqual(s1.__str__(), '[Square] (1) 6/2 - 4' )
