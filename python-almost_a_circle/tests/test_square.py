23296bad8f08b79a82dca4368d94c93c5e60f0af
Spacefox95 <nathan.raynal@gmail.com>
2024-02-22 12:37:24 +0100

    ok

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -101,3 +101,33 @@ class TestSquare(unittest.TestCase):
         """
         s1 = Square(4, 6, 2, 1)
         self.assertEqual(s1.__str__(), '[Square] (1) 6/2 - 4' )
+    
+    def test_to_dictionary(self):
+        """
+        Test the function that return a dict representation of square
+        """
+        s1 = Square(10, 2, 1, 9)
+        self.assertEqual(s1.to_dictionary(), {'id': 9, 'size': 10, 'x': 2, 'y': 1})
+
+    def test_update(self):
+        """
+        Test the that the update function exist
+        """
+        r = Square(1, 2, 3, 4)
+        r.update(id=6, size=7, x=9, y=10)
+        self.assertEqual(r.id, 6)
+        self.assertEqual(r.size, 7)
+        self.assertEqual(r.x, 9)
+        self.assertEqual(r.y, 10)
+    
+    def test_create(self):
+        """
+        Test that the create function exist
+        """
+        r1 = Square(id=89, size=1, x=3, y=4)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Square.create(**r1_dictionary)
+        self.assertEqual(r2.id, 89)
+        self.assertEqual(r2.size, 1)
+        self.assertEqual(r2.x, 3)
+        self.assertEqual(r2.y, 4)
