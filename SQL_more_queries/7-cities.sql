0b9ad376b12c1e20d9df56e92ca71fa4543861e2
Spacefox95 <nathan.raynal@gmail.com>
2024-03-19 10:08:06 +0000

    Foreign key

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Create a databse and another table 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id)
+		REFERENCES states(id)
+) ENGINE=INNODB;
