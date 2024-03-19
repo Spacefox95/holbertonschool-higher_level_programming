fb21f07d28acc9ebfc11f005fa142ce3a6b6f221
Spacefox95 <nathan.raynal@gmail.com>
2024-03-19 14:07:54 +0000

    ok

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,6 @@
+-- Use a database to list all genres of some show
+SELECT tv_shows.title FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id 
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
