18a7d9e2d8ed569760ba5706f74fd82a8d19a108
Spacefox95 <nathan.raynal@gmail.com>
2024-03-19 10:00:12 +0000

    New_state_table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Create a new databse with table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	name VARCHAR(256) NOT NULL
+);
