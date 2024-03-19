8c857b8fbc01f665ddd8bb02cc81b15ddc598947
Spacefox95 <nathan.raynal@gmail.com>
2024-03-19 12:53:39 +0000

    ok

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Lists all show contained in the database without a genre link
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
