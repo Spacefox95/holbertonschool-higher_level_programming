4a97cca34b38198e0bf749cab1601a60778246f6
Spacefox95 <nathan.raynal@gmail.com>
2024-03-20 09:16:24 +0000

    Force_name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,7 @@
- -- Create a new table 'force_name' on MySQL server
+ -- Create a new table 'force_name' on MySQL server if it does not exist
 CREATE TABLE IF NOT EXISTS force_name (
-	id INT,
-	name VARCHAR(256) NOT NULL
+    -- id column : type INT
+    id INT,
+    -- name column : type VARCHAR(256), cannot be NULL
+    name VARCHAR(256) NOT NULL
 );
