d337da0414e65fe1fedeea5a3197505114a6e019
Spacefox95 <nathan.raynal@gmail.com>
2024-03-21 11:36:34 +0100

    Filter state

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -13,10 +13,10 @@ if __name__ == "__main__":
     """
     db = MySQLdb.connect(
         host="localhost",
+        port=3306,
         user=sys.argv[1],
         password=sys.argv[2],
-        database=sys.argv[3],
-        port=3306)
+        database=sys.argv[3])
 
     cursor = db.cursor()
     cursor.execute(
