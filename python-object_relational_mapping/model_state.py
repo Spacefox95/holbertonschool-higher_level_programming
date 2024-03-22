8c0cba02f050a734dcbd29e16691218bc5da0e61
Spacefox95 <nathan.raynal@gmail.com>
2024-03-22 09:50:11 +0100

    Print_states

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -13,10 +13,11 @@ Base = declarative_base()
 class State(Base):
     """
     New class State as database
+    New table 'states'
     Args :
     id = INT UNIQUE PRIMARY KEY NOT NULL
     name = STRING(128) NO NULL
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
