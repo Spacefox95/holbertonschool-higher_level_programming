588d88a33c2c52cba87bad997e3d7eb86b31a3ff
Spacefox95 <nathan.raynal@gmail.com>
2024-03-22 10:39:30 +0100

    ok

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Lists all State objects from
+database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    """
+    Connecting to MySQL server
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    """
+    Create a session
+    """
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        if 'a' in str(state.name):
+            print("{}: {}".format(state.id, state.name))
+    session.close()
